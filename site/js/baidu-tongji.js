865b4213d7c383feaf2ec14d52eec6a895a6723c
FengchenHe <hefengchen0718@gmail.com>
2022-12-19 13:59:55 +0800

    auto build in 2022/12/19 13:59:55.51

---
+++ site/js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2f98f15986567a1aad8694aae6b8e4fb";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
